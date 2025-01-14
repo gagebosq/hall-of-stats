414bfc1c60c7b77648dbab6c5ef8601d936acfa2
swagatron300 <gage.bosquez@gmail.com>
2025-01-14 17:27:48 -0600

    initial spring and db setup

---
+++ src/main/java/com/hos/hall_of_stats/HallOfStatsApplication.java
@@ -0,0 +1,13 @@
+package com.hos.hall_of_stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HallOfStatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HallOfStatsApplication.class, args);
+    }
+
+}
