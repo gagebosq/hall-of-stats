6f6ea472df6e58015cc7d734c8d6d42127d63d94
swagatron300 <gage.bosquez@gmail.com>
2025-01-15 19:50:32 -0600

    added cors

--- src/main/java/com/hos/hall_of_stats/HallOfStatsApplication.java
+++ src/main/java/com/hos/hall_of_stats/HallOfStatsApplication.java
@@ -2,12 +2,22 @@ package com.hos.hall_of_stats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class HallOfStatsApplication {
+public class HallOfStatsApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(HallOfStatsApplication.class, args);
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000") // react URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
